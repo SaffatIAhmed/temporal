84d1dccd93086b42a65600781fd64baaa6844aa2
Vedang Javdekar <vedang.javdekar@gmail.com>
2023-12-10 17:20:54 -0600

    Header User react hooks order bug resolved

--- src/components/common/HeaderUser.tsx
+++ src/components/common/HeaderUser.tsx
@@ -7,47 +7,46 @@ import CreateListingModal from "../listings/CreateListingModal";
 import { NavLink } from "react-router-dom";
 
 function HeaderUser({ loggedIn, moderator }: HeaderData) {
-	const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+    if (!loggedIn || moderator) {
+        return null;
+    }
 
-	if (!loggedIn || moderator) {
-		return null;
-	}
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+    const dispatch = useContext(UserDispatchContext);
 
-	const dispatch = useContext(UserDispatchContext);
+    const handleLogout = async () => {
+        const result = await fetch("http://localhost:3000/users/logout", {
+            method: "post",
+        });
+        if (result.ok) {
+            dispatch({ type: UserActionKind.LOGOUT });
+        }
+    };
 
-	const handleLogout = async () => {
-		const result = await fetch("http://localhost:3000/users/logout", {
-			method: "post",
-		});
-		if (result.ok) {
-			dispatch({ type: UserActionKind.LOGOUT });
-		}
-	};
+    return (
+        <>
+            <button
+                className="nav-link"
+                onClick={() => setIsCreateModalOpen(true)}
+            >
+                Create Listing
+            </button>
+            <NavLink to={RouteNames.POSTED} className={"nav-link"}>
+                Posted
+            </NavLink>
+            <NavLink to={RouteNames.SAVED} className={"nav-link"}>
+                Saved
+            </NavLink>
+            <button className="nav-link" onClick={handleLogout}>
+                Logout
+            </button>
 
-	return (
-		<>
-			<button
-				className="nav-link"
-				onClick={() => setIsCreateModalOpen(true)}
-			>
-				Create Listing
-			</button>
-			<NavLink to={RouteNames.POSTED} className={"nav-link"}>
-				Posted
-			</NavLink>
-			<NavLink to={RouteNames.SAVED} className={"nav-link"}>
-				Saved
-			</NavLink>
-			<button className="nav-link" onClick={handleLogout}>
-				Logout
-			</button>
-
-			<CreateListingModal
-				showModal={isCreateModalOpen}
-				handleClose={() => setIsCreateModalOpen(false)}
-			/>
-		</>
-	);
+            <CreateListingModal
+                showModal={isCreateModalOpen}
+                handleClose={() => setIsCreateModalOpen(false)}
+            />
+        </>
+    );
 }
 
 export default HeaderUser;
